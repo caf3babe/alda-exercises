fb74258a4632038e1377e6ddec57300524e47260
Nichil <nichil.strasser@gmail.com>
2019-05-27 07:11:06 +0200

    done exercise4

---
+++ exercise4/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+int main() {
+
+    doubly_linked_list *list;
+    list = init();
+    insertEnd(list,1,"legage");
+    insertEnd(list,2,"legage");
+    insertEnd(list,3,"legage");
+    insertEnd(list,4,"legage");
+    insertEnd(list,12341234,"Das Leben ist so schoen! NICHT.");
+    insertEnd(list,5,"legage");
+    print(list);
+
+    printf("Searching for node with key 5 in list\n");
+    node* toFind = find(list,5);
+    printf("Key: %d\n",toFind->key);
+    printf("Data: %s\n",toFind->data);
+
+    printf("Attempting to delete node with key 12341234 from list\n");
+    delete(list,12341234);
+    print(list);
+
+    printf("Inserting new node before node with key 1\n");
+    insertBefore(list,69,"yeah",1);
+    print(list);
+
+    return 0;
+}
